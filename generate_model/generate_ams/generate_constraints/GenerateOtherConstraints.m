907950929b94af8904fdf346ac85815250cd13d9
Andrew <andrew.bollinger@empa.ch>
2016-11-14 11:36:54 +0100

    deleting stuff

--- generate_model/generate_ams/generate_constraints/GenerateOtherConstraints.m
+++
@@ -1,9 +0,0 @@
-%% GENERATE CARBON CONSTRAINTS
-
-%max allowable carbon constraint
-constraint_max_carbon = '';
-if apply_constraint_max_carbon == 1
-    constraint_max_carbon = '\n\t\tConstraint Maximum_carbon_emissions_constraint {\n\t\t\tDefinition: Total_carbon <= Carbon_emissions_limit;\n\t\t}';
-end
-
-constraints_section = strcat(constraints_section,constraint_max_carbon);
